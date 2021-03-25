e5d4eef25a488cbafae44070b48caaaef53d1bb6
xjossanc <jose.sanchez.tamborero@itbo-ericsson.com>
2021-03-25 10:34:42 +0100

    First commit

---
+++ src/main/java/com/ust/coffee/defecto/WiredBrainCoffeeService.java
@@ -0,0 +1,16 @@
+package com.ust.coffee.defecto;
+
+public class WiredBrainCoffeeService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Proyecto creado para Wired Brain Coffee");
+		
+		System.out.println("Curso Git - Getting Started with Git - Aaron Stewart");
+		
+		System.out.println("Curso Url - https://app.pluralsight.com/library/courses/getting-started-git/table-of-contents");
+		
+	}
+
+}
